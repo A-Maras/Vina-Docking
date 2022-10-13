33e1ccd231e15cd13614e39837550e2a166a7e02
Andrew Maras <72049888+A-Maras@users.noreply.github.com>
2022-10-13 01:19:28 +0000

    Ran docking, added ligands and receptors

--- autodock_vina.py
+++ autodock_vina.py
@@ -2,43 +2,52 @@ from vina import Vina
 import os
 from datetime import datetime
 
- 
+
 def create_dir(directory):
-  if not os.path.exists(directory):
-      print('Creating Directory '+directory)
-      os.makedirs(directory)
+    if not os.path.exists(directory):
+        print('Creating Directory ' + directory)
+        os.makedirs(directory)
+
 
 def vina_dock(receptor_file, ligand_file, map_profile):
-  # Create output folder for the energy minimization and docking files, plus the poses binding affinity 
-  creation_time = datetime.now()
-  create_dir('outputs/' + str(creation_time))
+    # Create output folder for the energy minimization and docking files, plus the poses binding affinity
+    creation_time = datetime.now()
+    create_dir('outputs/' + str(creation_time))
 
-  v = Vina(sf_name='vina')
-  
-  v.set_receptor('receptors/' + str(receptor_file) + '.pdbqt')
-  v.set_ligand_from_file('ligands/' +str(ligand_file) + '.pdbqt')
+    v = Vina(sf_name='vina')
 
-  # Compute vina maps
-  maps = map_profile.lower()
-  if maps == 'default':
-    v.compute_vina_maps(center=[0.,0.,0.], box_size=[30, 30, 30])
-  elif maps == 'testcase':
-    v.compute_vina_maps(center=[15.190,53.903,16.917], box_size=[20, 20, 20])
-  else:
-    print('unknown map profile: enter "default" or "testcase"')
-  
-  # Score the current pose
-  energy = v.score()
-  print('Score before minimization: %.3f (kcal/mol)' % energy[0])
+    v.set_receptor('receptors/' + str(receptor_file) + '.pdbqt')
+    v.set_ligand_from_file('ligands/' + str(ligand_file) + '.pdbqt')
 
-  # Create output folder for the energy minimization and docking files
+    # Compute vina maps
+    maps = map_profile.lower()
+    if maps == 'default':
+        v.compute_vina_maps(center=[0, 0, 0], box_size=[28, 12, 26], spacing=1.0)
+    elif maps == 'testcase':
+        v.compute_vina_maps(center=[15.190, 53.903, 16.917],
+                            box_size=[20, 20, 20])
+    elif maps == 'arylomycin':  #4.116,-7.565,4.912 - used for docking Arylomycin A2 30, 30, 30 size
+        v.compute_vina_maps(center=[4.116,-7.565,4.912],
+                            box_size=[30, 30, 30])
+    else:
+        print('unknown map profile: enter "default" or "testcase"')
 
-  
-  # Minimized locally the current pose
-  energy_minimized = v.optimize()
-  print('Score after minimization : %.3f (kcal/mol)' % energy_minimized[0])
-  v.write_pose('outputs/' + str(creation_time)+ '/' + str(receptor_file) + '_' + str(ligand_file) + '_vina_minimized.pdbqt', overwrite=True)
+    # Score the current pose
+    energy = v.score()
+    print('Score before minimization: %.3f (kcal/mol)' % energy[0])
 
-  # Dock the ligand
-  v.dock(exhaustiveness=32, n_poses=20)
-  v.write_poses('outputs/' + str(creation_time)+ '/' + str(receptor_file) + '_' + str(ligand_file) + '_vina.pdbqt', n_poses=5, overwrite=True)
+    # Create output folder for the energy minimization and docking files
+
+    # Minimized locally the current pose
+    energy_minimized = v.optimize()
+    print('Score after minimization : %.3f (kcal/mol)' % energy_minimized[0])
+    v.write_pose('outputs/' + str(creation_time) + '/' + str(receptor_file) +
+                 '_' + str(ligand_file) + '_vina_minimized.pdbqt',
+                 overwrite=True)
+
+    # Dock the ligand
+    v.dock(exhaustiveness=32, n_poses=20)
+    v.write_poses('outputs/' + str(creation_time) + '/' + str(receptor_file) +
+                  '_' + str(ligand_file) + '_vina.pdbqt',
+                  n_poses=5,
+                  overwrite=True)
--- main.py
+++ main.py
@@ -3,11 +3,14 @@ import autodock_vina
 import pdb_coord_parse
 
 # Meeko: Preparation of pdbqt files, mainly for ligands
-meeko_prep.pdbqt_prep('BAL4850C')
+#meeko_prep.pdbqt_prep('BAL4850C')
+
 
 # Autodock Vina: Molecular Docking
-#autodock_vina.vina_dock('1b12','44359165','default')
-#autodock_vina.vina_dock('4wvg','PK150','default')
+#autodock_vina.vina_dock('1b12','BAL4850C','default')
+#autodock_vina.vina_dock('4wvg','pk150','default')
+#autodock_vina.vina_dock('1kn9','BAL4850C','default')
+
 
 # PDB Coordinate Parsing
 #pdb = pdb_coord_parse.parsePDB('1ZG4.pdb')
